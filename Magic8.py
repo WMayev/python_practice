1d004f9933f03a0157266938dbc3bd72fc18be2e
Werka <wm.administracja@proton.me>
2025-04-27 22:01:11 +0200

    Add variables

--- Magic8.py
+++ Magic8.py
@@ -2,3 +2,7 @@
 Write a magic8.py Python program that can answer any “Yes” or “No” 
 question with a different fortune each time it executes.
 '''
+
+name = "Eddie"  # Name of person who ask a question
+question = "Will mom buy me a toy?" # Question to ask the Magic 8-Ball
+anser = "" # The Magic 8-Ball's answer
