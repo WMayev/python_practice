dcbc0421358a32f5d2acf2c710583d5555fbe04f
Werka <wm.administracja@proton.me>
2025-04-27 22:36:02 +0200

    Test

--- Magic8.py
+++ Magic8.py
@@ -3,8 +3,8 @@ Write a magic8.py Python program that can answer any “Yes” or “No”
 question with a different fortune each time it executes.
 '''
 
-name = "Eddie"  # Name of person who ask a question
-question = "Will mom buy me a toy?" # Question to ask the Magic 8-Ball
+name = ""  # Name of person who ask a question
+question = "" # Question to ask the Magic 8-Ball
 answer = "" # The Magic 8-Ball's answer
 
 import random 
