25cd75af0848e88b277c557e1511cfb1c97e7ecf
Werka <wm.administracja@proton.me>
2025-04-27 22:07:07 +0200

    Import the random module and add random_number variable

--- Magic8.py
+++ Magic8.py
@@ -7,3 +7,8 @@ name = "Eddie"  # Name of person who ask a question
 question = "Will mom buy me a toy?" # Question to ask the Magic 8-Ball
 answer = "" # The Magic 8-Ball's answer
 
+import random 
+# Import a module to use its functions
+
+random_number = random.randint(1, 9)
+# Generate a random number between 1 (inclusive) and 9 (inclusive)
